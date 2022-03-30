b2df63e95ad40a51a27321e6f326de84a5568773
fed1k <allamurotovcoder@mail.ru>
2022-03-31 01:19:50 +0500

    complete task4

---
+++ task4/task4.js
@@ -0,0 +1,14 @@
+const capitalize = (string) =>{
+    let capitalized = '';
+    let lowerCases = '';
+    for(let i=0;i<string.length;i += 1){
+        if(i === 0){
+            capitalized += string[0];
+        }else{
+            lowerCases += string[i];
+        }
+    }
+    return capitalized.toUpperCase() + lowerCases;
+}
+// console.log(capitalize('salom'));
+module.exports = capitalize
---
+++ task4/task4.test.js
@@ -0,0 +1,5 @@
+const capitalize = require('./task4');
+
+test('First Letter Capitalizer', ()=>{
+    expect(capitalize('firdavs')).toBe('Firdavs')
+})
